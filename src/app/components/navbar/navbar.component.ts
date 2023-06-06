dc6b2b1d611191bcbf3f80445679b7c3903ffb47
broni <broniekf@gmail,com>
2023-06-06 22:44:42 +0200

    handle logout button

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-navbar',
@@ -7,5 +8,16 @@ import {Router} from "@angular/router";
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  constructor(private authService: AuthService, private router: Router) {
+  }
 
+  logout() {
+    this.authService.logout().subscribe(result => {
+      this.router.navigate(['/']);
+    });
+  }
+
+  isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
 }
